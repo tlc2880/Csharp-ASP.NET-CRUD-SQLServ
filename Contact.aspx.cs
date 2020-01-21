c17a6732c611285d0beb9ba37932e9fdb5510613
Tommy Cao <gevh2880@gmail.com>
2020-01-20 16:22:53 -0800

    first commit

---
+++ Contact.aspx.cs
@@ -0,0 +1,112 @@
+﻿//  Name: Tommy Cao
+//  Date: 04/17/15
+//  Title: C# Asp.Net-Insert Update Delete and View With SQL Server Database
+//  Description:  Implementation asp.net application with CRUD Create, Review, Update, and Delete with SQL server database using a stored procedure.
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace asp.netcrud
+{
+    public partial class Contact : System.Web.UI.Page
+    {
+
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=CAOPC-19;Initial Catalog=ASPCRUD;Integrated Security=true;");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btnDelete.Enabled = false;
+                FillGridView();
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            hfContactID.Value = "";
+            txtName.Text = txtMobile.Text = txtAddress.Text = "";
+            lblSuccessMessage.Text = lblErrorMessage.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand("ContactCreateOrUpdate",sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@ConatctID",(hfContactID.Value==""?0:Convert.ToInt32(hfContactID.Value)));
+            sqlCmd.Parameters.AddWithValue("@Name",txtName.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@Mobile",txtMobile.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            string contactID = hfContactID.Value;
+            Clear();
+            if (contactID == "")
+                lblSuccessMessage.Text = "Saved Successfully";
+            else
+                lblSuccessMessage.Text = "Updated Successfully";
+            FillGridView();
+        }
+
+        void FillGridView()
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("ContactViewAll",sqlCon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dtbl = new DataTable();
+            sqlDa.Fill(dtbl);
+            sqlCon.Close();
+            gvContact.DataSource = dtbl;
+            gvContact.DataBind();
+        
+        }
+
+        protected void lnk_OnClick(object sender, EventArgs e)
+        {
+            int contactID = Convert.ToInt32((sender as LinkButton).CommandArgument);
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("ContactViewByID", sqlCon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDa.SelectCommand.Parameters.AddWithValue("@ContactID", contactID);
+            DataTable dtbl = new DataTable();
+            sqlDa.Fill(dtbl);
+            sqlCon.Close();
+            hfContactID.Value = contactID.ToString();
+            txtName.Text = dtbl.Rows[0]["Name"].ToString();
+            txtMobile.Text = dtbl.Rows[0]["Mobile"].ToString();
+            txtAddress.Text = dtbl.Rows[0]["Address"].ToString();
+            btnSave.Text = "Update";
+            btnDelete.Enabled = true;
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand("ContactDeleteByID",sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@ContactID",Convert.ToInt32(hfContactID.Value));
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            Clear();
+            FillGridView();
+            lblSuccessMessage.Text = "Deleted Successfully";
+        }
+    }
+}
---
+++ bin/Release/Publish/Contact.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="asp.netcrud.Contact" %>
+
+<!DOCTYPE html>
+
+<!--
+//  Name: Tommy Cao
+//  Date: 04/17/15
+//  Title: C# Asp.Net-Insert Update Delete and View With SQL Server Database
+//  Description:  Implementation asp.net application with CRUD Create, Review, Update, and Delete with SQL server database using a stored procedure.
+-->
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <%--  --%>
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:HiddenField ID="hfContactID" runat="server" />
+        <table>
+            <tr>
+                <td>
+                    <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="Label2" runat="server" Text="Mobile"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtMobile" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="Label3" runat="server" Text="Adddress"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtAddress" runat="server" TextMode="MultiLine"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    
+                </td>
+                <td colspan="2">
+                    <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+                    <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                    <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    
+                </td>
+                <td colspan="2">
+                    <asp:Label ID="lblSuccessMessage" runat="server" Text="" ForeColor="Green"></asp:Label>
+                </td>
+                <tr>
+                <td>
+                    
+                </td>
+                <td colspan="2">
+                    <asp:Label ID="lblErrorMessage" runat="server" Text="" ForeColor="Red"></asp:Label>
+                </td>
+            </tr>
+            </tr>
+        </table>
+        <br />
+        <asp:GridView ID="gvContact" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="Name" HeaderText="Name" />
+                <asp:BoundField DataField="Mobile" HeaderText="Mobile" />
+                <asp:BoundField DataField="Address" HeaderText="Address" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkView" runat="server" CommandArgument='<%# Eval("ContactID") %>' OnClick="lnk_OnClick">View</asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
